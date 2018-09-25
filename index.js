1ed7d47b4944b28d2ac4d88cc3a014c2a2625a40
kanwalgupta <kanwal.gupta1@gmail.com>
2018-09-25 06:27:55 +0000

    Refactor

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ function newTask(title, description) {
   return task;
 }
 
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);
 
 
